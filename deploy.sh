adf9b1b5ac0cfe79236b56bd6db2f4d0e6660b2c
talithafrsc <talithafresca@gmail.com>
2024-10-28 00:24:21 +0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 reload_nginx() {
     docker exec nginx /usr/sbin/nginx -s reload
 }
@@ -10,7 +12,7 @@ OLD_CONTAINER=$(docker ps --format "{{.Names}}" | grep petclinic)
 
 # Deploy new container
 docker compose up -d --no-deps --scale petclinic-app=2 --no-recreate petclinic-app
-reload_nginx()
+reload_nginx
 
 # Get new running petclinic container name
 NEW_CONTAINER=$(docker ps --latest --format "{{.Names}}")
@@ -29,8 +31,8 @@ done
 # Stopping the old container
 docker stop $OLD_CONTAINER
 docker rm $OLD_CONTAINER
-reload_nginx()
+reload_nginx
 
 # Scale down the container
 docker compose up -d --no-deps --scale petclinic-app=1
-reload_nginx()
+reload_nginx
